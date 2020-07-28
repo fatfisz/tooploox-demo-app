71d6adf169c5d7ddb0a8db69a9d4c494bcd6ab2c
fatfisz <fatfisz@gmail.com>
2020-07-28 22:52:49 +0200

    Fix Jest TS configuration after Next.js auto change

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,13 @@
 'use strict';
 
 module.exports = {
+  globals: {
+    'ts-jest': {
+      tsConfig: {
+        jsx: 'react',
+      },
+    },
+  },
   moduleDirectories: ['node_modules', 'src'],
   moduleNameMapper: {
     '\\.svg': '<rootDir>/__mocks__/svgMock.tsx',
@@ -9,4 +16,5 @@ module.exports = {
   restoreMocks: true,
   setupFilesAfterEnv: ['./jest.setup.tsx'],
   testPathIgnorePatterns: ['<rootDir>/\\.next/', '<rootDir>/node_modules/'],
+  timers: 'fake',
 };

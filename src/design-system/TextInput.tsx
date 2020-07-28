71daa3018c02f8a1dec61a311affc2f9f52cf8da
fatfisz <fatfisz@gmail.com>
2020-07-28 17:17:13 +0200

    Implement the Input component

--- src/design-system/TextInput.tsx
+++ src/design-system/TextInput.tsx
@@ -1,5 +1,7 @@
 import { ChangeEvent as ReactChangeEvent, ReactElement, useCallback } from 'react';
 
+import { useTextStyles, useTheme } from 'design-system/Theme';
+
 export function TextInput({
   placeholder,
   search,
@@ -11,17 +13,42 @@ export function TextInput({
   value: string;
   onChange: (value: string) => void;
 }): ReactElement {
+  const theme = useTheme();
+  const inputStyles = useTextStyles('basic');
   const handleChange = useCallback(
     (event: ReactChangeEvent<HTMLInputElement>) => onChange(event.target.value),
     [onChange],
   );
   return (
-    <input
-      placeholder={placeholder}
-      type="text"
-      value={value}
-      onChange={handleChange}
-      data-search={search}
-    />
+    <>
+      <div className="input-wrapper">
+        <input
+          className={inputStyles.className}
+          placeholder={placeholder}
+          type="text"
+          value={value}
+          onChange={handleChange}
+          data-search={search}
+        />
+      </div>
+      <style jsx>{`
+        .input-wrapper {
+          background-color: ${theme.color.inputBackground};
+          border-radius: ${theme.borderRadius.basic};
+          height: ${theme.size.control};
+          position: relative;
+        }
+
+        input {
+          background-color: transparent;
+          border: none;
+          color: ${theme.color.textDark};
+          height: 100%;
+          padding: 0 ${theme.spacing.xsmall};
+          width: 100%;
+        }
+      `}</style>
+      {inputStyles.styles}
+    </>
   );
 }

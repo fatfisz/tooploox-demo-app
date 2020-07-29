83434eccfc687b01846d9bbb987c3a74dd0850d4
fatfisz <fatfisz@gmail.com>
2020-07-29 17:43:37 +0200

    Add a page title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 import { ReactElement } from 'react';
 
 import { ThemeHead } from 'design-system';
@@ -6,6 +7,9 @@ import { ThemeHead } from 'design-system';
 export default function App({ Component, pageProps }: AppProps): ReactElement {
   return (
     <>
+      <Head>
+        <title>GitHub profile viewer</title>
+      </Head>
       <ThemeHead />
       <Component {...pageProps} />
     </>

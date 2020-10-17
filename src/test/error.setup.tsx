f17efec1eb20998e22f267a0dc5727641211a001
fatfisz <fatfisz@gmail.com>
2020-10-18 01:32:24 +0200

    Remove unused styled-jsx stuff

--- src/test/error.setup.tsx
+++ src/test/error.setup.tsx
@@ -10,11 +10,6 @@ beforeAll(() => {
 });
 
 const consoleErrorFilters: ((...args: any[]) => boolean)[] = [
-  // Silence React complaining about styled-jsx, as it's not transpiled for the tests
-  (...args): boolean =>
-    /Warning: Received `%s` for a non-boolean attribute `%s`./.test(args[0]) &&
-    args[1] === 'true' &&
-    args[2] === 'jsx',
   // Silence dumb error boundary errors
   (...args): boolean => /Error boundaries should implement getDerivedStateFromError/.test(args[0]),
   (...args): boolean => /React will try to recreate this component tree/.test(args[0]),
